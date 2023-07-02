f5823295d7bd22e134ec4fe4c48d4d2531dc5a78
Ola Flisbäck <ola.flisback@gmail.com>
2023-07-02 12:03:50 +0200

    Basic neorg config

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "nvim-neorg/neorg",
+    lazy = false,
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
+      })
+    end,
+  },
+}
